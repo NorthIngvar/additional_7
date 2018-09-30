913d7f149d97f4269e0100fecd6acd6db6e73147
Killjoy4 <dedovich1.17@gmail.com>
2018-09-30 14:09:17 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  
+  for (let a = 0; a < 9; a++) {
+    for (let b = 0; b < 9; b++) {
+      if (matrix[a][b] === 0) {
+        for (let temporaryNumber = 1; temporaryNumber <= 9; temporaryNumber++) {
+          if (temporaryFunc(matrix, a, b, temporaryNumber)) {
+            matrix[a][b] = temporaryNumber;
+            if (solveSudoku(matrix)) {
+              return matrix;
+            }
+            matrix[a][b] = 0;
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function temporaryFunc(matrix, temporaryRow, temporaryCol, temporaryNumber) {
+  let mainRow = Math.floor(temporaryRow / 3) * 3;
+  let mainCol = Math.floor(temporaryCol / 3) * 3;
+
+  
+  for (let col = 0; col < 9; col++) {
+    if (col != temporaryCol && matrix[temporaryRow][col] === temporaryNumber) {
+      return false;
+    }
+  }
+
+
+  for (let row = 0; row < 9; row++) {
+    if (row != temporaryRow && matrix[row][temporaryCol] === temporaryNumber) {
+      return false;
+    }
+  }
+
+
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (row != temporaryRow && col != temporaryCol && matrix[mainRow + row][mainCol + col] === temporaryNumber) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
